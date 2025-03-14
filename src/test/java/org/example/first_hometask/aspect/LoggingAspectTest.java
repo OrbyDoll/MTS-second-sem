431e6c6798b3b2d4a0ab0b239ba71f32484afae0
OrbyDoll <antonkhazin2006@gmail.com>
2025-03-14 12:44:55 +0300

    attempt 2 fix AspectTest

--- src/test/java/org/example/first_hometask/aspect/LoggingAspectTest.java
+++ src/test/java/org/example/first_hometask/aspect/LoggingAspectTest.java
@@ -1,30 +1,32 @@
-// Этот тест проходит у меня локально, но на гитхабе падает с ошибкой FailedInitializeApplicationContext
-// Я не смог пофиксить поэтому пришлось закомментить(
+package org.example.first_hometask.aspect;
 
+import org.example.first_hometask.Application;
+import org.example.first_hometask.controller.UsersController;
+import org.example.first_hometask.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-//package org.example.first_hometask.aspect;
-//
-//import org.example.first_hometask.controller.UsersController;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//@ActiveProfiles("application-test")
-//public class LoggingAspectTest {
-//
-//  @Autowired
-//  private UsersController userController;
-//
-//  @Autowired
-//  private LoggingAspect loggingAspect;
-//
-//  @Test
-//  public void testAspectCounter() {
-//    userController.getAllUsers();
-//    assertEquals(2, loggingAspect.getCounter());
-//  }
-//}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ContextConfiguration(classes = {Application.class, SecurityConfig.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ActiveProfiles("application-test")
+public class LoggingAspectTest {
+
+  @Autowired
+  private UsersController userController;
+
+  @Autowired
+  private LoggingAspect loggingAspect;
+
+  @Test
+  public void testAspectCounter() {
+    userController.getAllUsers();
+    assertEquals(2, loggingAspect.getCounter());
+  }
+}
